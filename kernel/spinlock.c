8657553dc0597ee307ec8c5db2e382f291bafb81
Niko <niko@local>
2025-04-18 17:08:04 +0300

    __ATOMIC_* and sleep instr

--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -3,12 +3,15 @@
 void
 spin_lock(spinlock_t *lock)
 {
-    while (__sync_lock_test_and_set(&lock->locked, 1))
-        ;
+    // __ATOMIC_ACQUIRE ensures no memory reordering before the lock is acquired
+    while (__atomic_test_and_set(&lock->locked, __ATOMIC_ACQUIRE))
+        asm volatile ("pause");
 }
 
 void
 spin_unlock(spinlock_t *lock)
 {
-    __sync_lock_release(&lock->locked);
+    // __ATOMIC_RELEASE ensures all writes inside the critical section complete
+    // before the lock is released
+    __atomic_clear(&lock->locked, __ATOMIC_RELEASE);
 }
